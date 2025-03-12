d43276bcb48cf7ddee98f1de4cde88e3d27e600c
FreezingH2O <airadajitauleeruk@gmail.com>
2025-03-12 13:35:51 +0700

    add url

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image";
 import styles from './topMenu.module.css'
 import TopMenuItem from "./TopMenuItem";
+import Link from "next/link";
 
 export default function TopMenu(){
     return (
         <div className={styles.topMenu}>
             <TopMenuItem title="Booking" pageRef="/booking"/>
+            <Link href='..'>
             <Image className={styles.logo}
                 src = '/img/logo.png'
                 alt = "logo"
-                width= {0}
-                height = {0}
-                sizes="100vh"
+                width= {48}
+                height = {48}
+         
             />
+            </Link>
         </div>
     );
 }
